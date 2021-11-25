da010160ea40788ab62c7e20efda508214756430
doppal <92789568+doppal@users.noreply.github.com>
2021-11-25 18:13:31 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,22 +14,21 @@ provider "aws" {
 resource "aws_vpc" "ownvpc" {
   #cidr_block = "10.0.0.0/16"
   cidr_block = var.vpc_cidr_block
-  tags={
+  tags = {
     Name="${var.env}-vpc"
   }
 }
-module "myserver-subnet"{
-  source ="./modules/subnet"
-  vpc_id =aws_vpc.ownvpc.id
+module "myserver-subnet" {
+  source = "./modules/subnet"
+  vpc_id = aws_vpc.ownvpc.id
   subnet_cidr_block = var.subnet_cidr_block
   env = var.env
   az = var.az
 }
-
-module "myserver-instance"{
-    source ="./modules/webserver"
-    vpc_id =aws_vpc.ownvpc.id
-    subnet_id = module.myserver-subnet.subnet.id
-    env = var.env
-    instance_type = var.instance_type
+module "myserver-instance" {
+  source = "./modules/webserver"
+  vpc_id = aws_vpc.ownvpc.id
+  subnet_id = module.myserver-subnet.subnet.id
+  env = var.env
+  instance_type = var.instance_type
 }
