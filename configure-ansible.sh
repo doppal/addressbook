4f8f22296bdbdd3ec31fdb6d626d1990af48a0a2
[doppal] <[anudoppal@gmail.com]>
2021-12-05 16:32:04 +0530

    updated

--- configure-ansible.sh
+++ configure-ansible.sh
@@ -2,5 +2,5 @@
 pip3 install ansible==2.10.7
 /home/user1/.local/bin/ansible --version
 pip3 install boto3
-/home/user1/.local/bin/ansible-inventory -i inventory_aws_ec2.yml
+/home/user1/.local/bin/ansible-inventory -i inventory_aws_ec2.yml --list
 /home/user1/.local/bin/ansible-playbook docker-deploy.yml
